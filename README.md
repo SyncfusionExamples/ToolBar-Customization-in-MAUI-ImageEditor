f83188e6d488dc4f3a46b4a16e022e798c60a387
snehakPV <sneha.sf4154@gmail.com>
2024-09-20 17:10:09 +0530

    Updated sample and read me file

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # ToolBar-Customization-in-MAUI-ImageEditor
-This sample demonstrates how to customize the ToolBar in MAUI ImageEditor control within a .NET MAUI application.
+
+This sample demonstrates how to customize the ToolBar in MAUI ImageEditor control within a .NET MAUI application. You'll also see how to replace toolbar items with images, disable toolbar items, adjust the orientation, create a custom toolbar, and modify the toolbar's appearance.
+
+## Sample
+
+```xaml
+<imageEditor:SfImageEditor x:Name="imageEditor" 
+                           Source="scenery.png"
+                           AutoGenerateToolbarItems="False"
+                           ToolbarItemSelected="OnToolbarItemSelected">
+    
+    <imageEditor:SfImageEditor.Toolbars>
+        <imageEditor:ImageEditorToolbar Position="End" Orientaion="Vertical">
+            <imageEditor:ImageEditorToolbar.ToolbarItems>
+                <imageEditor:ImageEditorToolbarGroupItem Alignment="Start">
+                    <imageEditor:ImageEditorToolbarGroupItem.Items>
+                        <imageEditor:ImageEditorToolbarItem Name="Browse" />
+                        <imageEditor:ImageEditorToolbarItem Name="Reset" />
+                    </imageEditor:ImageEditorToolbarGroupItem.Items>
+                </imageEditor:ImageEditorToolbarGroupItem>
+                <imageEditor:ImageEditorToolbarGroupItem Alignment="End">
+                    <imageEditor:ImageEditorToolbarGroupItem.Items>
+                        <imageEditor:ImageEditorToolbarItem Name="Save" />
+                        <imageEditor:ImageEditorToolbarItem Name="SaveEdit" />
+                    </imageEditor:ImageEditorToolbarGroupItem.Items>
+                </imageEditor:ImageEditorToolbarGroupItem>
+            </imageEditor:ImageEditorToolbar.ToolbarItems>
+        </imageEditor:ImageEditorToolbar>
+    </imageEditor:SfImageEditor.Toolbars>
+
+</imageEditor:SfImageEditor>
+```
+
+## Requirements to run the demo
+
+To run the demo, refer to [System Requirements for .NET MAUI](https://help.syncfusion.com/maui/system-requirements)
+
+## Troubleshooting:
+### Path too long exception
+
+If you are facing path too long exception when building this example project, close Visual Studio and rename the repository to short and build the project.
+
+## License
+
+Syncfusion has no liability for any damage or consequence that may arise from using or viewing the samples. The samples are for demonstrative purposes. If you choose to use or access the samples, you agree to not hold Syncfusion liable, in any form, for any damage related to use, for accessing, or viewing the samples. By accessing, viewing, or seeing the samples, you acknowledge and agree Syncfusion's samples will not allow you seek injunctive relief in any form for any claim related to the sample. If you do not agree to this, do not view, access, utilize, or otherwise do anything with Syncfusion's samples.
---
+++ ToolBarSample/ToolBarSample/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Syncfusion.Maui.ImageEditor;
+
+namespace ToolBarSample
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+
+            imageEditor.ToolbarSettings.ColorPalette.Add(Colors.Yellow);
+            imageEditor.ToolbarSettings.ColorPalette.Add(Colors.Pink);
+            imageEditor.ToolbarSettings.ColorPalette.Add(Colors.Violet);
+
+            Image browseImage = new Image()
+            {
+                Source = ImageSource.FromFile("browse.png"),
+                HeightRequest = 30,
+                WidthRequest = 30
+            };
+
+            ImageEditorToolbar headerToolbar = imageEditor.Toolbars[0];
+            ImageEditorToolbarGroupItem toolbarGroup = (ImageEditorToolbarGroupItem)headerToolbar.ToolbarItems[0];
+            ImageEditorToolbarItem browseItem = toolbarGroup.Items.FirstOrDefault(i => i.Name == "Browse");
+            browseItem.View = browseImage;
+            
+            Image textImage = new Image()
+            {
+                Source = ImageSource.FromFile("text.png"),
+                HeightRequest = 30,
+                WidthRequest = 30
+            };
+
+            ImageEditorToolbar footerToolbar = imageEditor.Toolbars[1];
+            ImageEditorToolbarItem textItem = (ImageEditorToolbarItem)footerToolbar.ToolbarItems.FirstOrDefault(i => i.Name == "Text");
+            textItem.IsEnabled = false;
+
+            Image shareImage = new Image()
+            {
+                Source = ImageSource.FromFile("share.png"),
+                HeightRequest = 30,
+                WidthRequest = 30
+            };
+
+            ImageEditorToolbar headerToolbarItem = imageEditor.Toolbars[0];
+            ImageEditorToolbarGroupItem shareGroup = (ImageEditorToolbarGroupItem)headerToolbarItem.ToolbarItems[1];
+            shareGroup.Items.Add(new ImageEditorToolbarItem()
+            {
+                Name = "Share",
+                View = shareImage
+            });
+
+            ImageEditorToolbar footerToolbarItem = this.imageEditor.Toolbars[1];
+            ImageEditorToolbarItem cropItem = (ImageEditorToolbarItem)footerToolbarItem.ToolbarItems.FirstOrDefault(i => i.Name == "Crop");
+            cropItem.SubToolbarOverlay = false;
+            cropItem.SubToolbars = new List<ImageEditorToolbar>()
+            {
+                new ImageEditorToolbar()
+                {
+                    ToolbarItems = new List<IImageEditorToolbarItem>()
+                    {
+                        new ImageEditorToolbarItem(){ Name = "original"},
+                        new ImageEditorToolbarItem(){ Name = "square"},
+                        new ImageEditorToolbarItem(){ Name = "custom"}
+                    }
+                }
+            };
+        }
+
+        private void OnToolbarItemSelected(object sender, ToolbarItemSelectedEventArgs e)
+        {
+            DisplayAlert("Message", "Selected ToolbarItem is  " + e.ToolbarItem.Name, "Cancel", "Ok");
+        }
+
+    }
+}
